4f436a678f10916dc5346ae13ecf33a5c9f08b59
Khaleel ibn Anwar Mughal <khaleelm@gmail.com>
2016-05-15 11:28:55 +0400

    Update Genealogy.js

--- Genealogy.js
+++ Genealogy.js
@@ -1,25 +1,18 @@
 /*
-KHALEEL SYSTEMS
- - One day I will use this
- - inshAllah this is correct as per family research
+KHALEEL MUGHAL ABU MUHAMMAD
 */
 
 var name  = 'Khaleel Mughal';
- name += ' ibn';
- name += ' Anwar Mughal';
- name += ' ibn';
- name += ' Muhammed Mustaaq';
- name += ' ibn';
- name += ' Imam Deen';
- name += ' ibn';
- name += ' Barkat Ali';
- name += ' ibn';
- name += ' Karim Bux';
- name += ' ibn';
- name += ' Ibrahim';
- name += ' ibn';
- name += ' Akeel';
- console.log(name);
- 
- 
- 
+name += ' ibn';
+name += ' Anwar Mughal';
+name += ' ibn';
+name += ' Muhammad Mustaaq';
+name += ' ibn';
+name += ' Imam Deen';
+name += ' ibn';
+name += ' Karim Bux';
+name += ' ibn';
+name += ' Ibrahim';
+name += ' ibn';
+name += ' Akeel';
+console.log(name);
