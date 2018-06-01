13a17cf8cfaa9422b10a32b04b896da27cc6df74
Khaleel <khaleelm@gmail.com>
2018-06-01 11:13:41 +0400

    Added ES6 Sugar Syntax and corrected order
    Karim Bux is not part of this tree.

--- Genealogy.js
+++ Genealogy.js
@@ -2,15 +2,13 @@
 KHALEEL MUGHAL ABU MUHAMMAD
 */
 
-var name  = 'Khaleel Mughal';
+let name  = 'Khaleel Mughal';
 name += ' ibn';
 name += ' Anwar Mughal';
 name += ' ibn';
 name += ' Muhammad Mustaaq';
 name += ' ibn';
 name += ' Imam Deen';
-name += ' ibn';
-name += ' Karim Bux';
 name += ' ibn';
 name += ' Ibrahim';
 name += ' ibn';
